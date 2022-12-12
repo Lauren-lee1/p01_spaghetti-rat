e4c77b8a745bd2b3f3686a9c24b788406870460a
Lauren Lee <llee30@stuy.edu>
2022-12-12 18:33:59 -0500

    more than one optional

--- app/db.py
+++ app/db.py
@@ -449,6 +449,10 @@ def match_height(user, other_user):
     low_height = c.execute("SELECT low_heignt FROM pref WHERE user =?", (user,)).fetchone()
     high_height = c.execute("SELECT high_heignt FROM pref WHERE user =?", (user,)).fetchone()
 
+    if height >= low_height and height <= high_height:
+        return True
+    return False
+
 '''
 matching criteria:
 - mbti (optional)
@@ -542,34 +546,91 @@ def match(user, other_user):
                 shared_hobby = shared_hobby + 30
             if match_hobbies(user, x) == 2:
                 shared_hobby = shared_hobby + 20
-            matches[other_name] = shared_hobby + love_calc
+            matches[x] = ((shared_hobby + love_calc) / float(150.0)) * 100
     #============ all optional selected ====================#
-    if user_star_sign == 1 and use_mbti == 1 and low_height is None and high_height is None:
+    if use_star_sign == 1 and use_mbti == 1 and low_height is not None and high_height is not None:
         for x in total:
             #star sign
             star_sign_score = 0
             if match_star_sign(user, x) == 1:
-                star_sign = 15
+                star_sign_score = 15
             if match_star_sign(user, x) == -1:
-                star_sign = -10
+                star_sign_score = -10
             #mbti
             mbti_score = 0
             if match_mbti(user, x) == 1:
-                mbti = 20
+                mbti_score = 20
             if match_mbti(user, x) == -1:
-                mbti = -10
+                mbti_score = -10
             #height 
-
+            height_score = 0
+            if match_height:
+                height_score = 20
             #hobby
             shared_hobby = 0
             if match_hobbies(user, x) == 1:
                 shared_hobby = 10
             if match_hobbies(user, x) == 2:
                 shared_hobby = 25
+            #love calculator
+            other_name = c.execute("SELECT name FROM profile WHERE user=?", (x)).fetchone()
+            love_calc = 0.2 * api.love_calculator(name, other_name)
+            #total
+            matches[x] = ((star_sign_score + mbti_score + height_score + shared_hobby + love_calc) / float(180.0)) * 100
+    #================= any one is optional ======================#
+    optional = [15, 20, 20]
+    options = [True, True, True]
+    if use_star_sign == 0 or use_mbti == 0 or low_height is None and high_height is None:
+        if use_star_sign == 0 :
+            options[0] = False
+        if use_mbti == 0:
+            options[1] = False
+        if low_height is None:
+            options[2] = False
+        points = 0
+        counter = 0
+        for x in range(3):
+            if options[x] == False:
+                counter = counter + 1
+                points = points + optional[x]   
+        for x in total:
+            #star sign
+            star_sign_score = 0
+            if use_star_sign == 1:
+                if match_star_sign(user, x) == 1:
+                    star_sign_score = 15
+                if match_star_sign(user, x) == -1:
+                    star_sign_score = -10
+            #mbti           
+            mbti_score = 0
+            if use_mbti == 1:
+                if match_mbti(user, x) == 1:
+                    mbti_score = 20
+                if match_mbti(user, x) == -1:
+                    mbti_score = -10
+            #height
+            height_score = 0
+            if low_height is not None:
+                if match_height:
+                    height_score = 20
+            #hobby
+            shared_hobby = 0
+            if match_hobbies(user, x) == 1:
+                shared_hobby = 10
+            if match_hobbies(user, x) == 2:
+                shared_hobby = 25
+            #love calculator
+            other_name = c.execute("SELECT name FROM profile WHERE user=?", (x)).fetchone()
+            #division
+            points = points / float(5 - counter)
+            shared_hobby = shared_hobby + points
+            height_score = height_score + points
+            mbti_score = mbti_score + points
+            star_sign_score = star_sign_score + points
+            love_calc = (0.2 + (points/100)) * api.love_calculator(name, other_name)
+            matches[x] = ((star_sign_score + mbti_score + height_score + shared_hobby + love_calc) / float(star_sign_score + mbti_score + height_score + shared_hobby + love_calc + (points /(5  * (5-counter+1))) + (0.2 + (points/100)))) * 100
 
-            
-            
-
+            #star_sign_score + mbti_score + height_score + shared_hobby + love_calc + (points /(5  * (5-counter+1))) + (0.2 + (points/100))* 100
 
 
 # DB_FILE="test.db"

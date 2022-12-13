0b4099f4e463e793f4a156c2e3ef3718f318d907
Lauren Lee <llee30@stuy.edu>
2022-12-13 12:51:18 -0500

    keys

--- app/api.py
+++ app/api.py
@@ -2,12 +2,17 @@ import requests
 import json
 
 def love_calculator(name1, name2):
+
+    key = ""
+    with open("keys/key_love_calculator.txt", "r") as file:
+    key = file.read()
+
     url = "https://love-calculator.p.rapidapi.com/getPercentage"
 
     querystring = {"sname":name1,"fname":name2}
 
     headers = {
-        "X-RapidAPI-Key": "7de29e7ccemshe5f10ce923b4d08p19b4c0jsn2e56c233db7a",
+        "X-RapidAPI-Key": "",
         "X-RapidAPI-Host": "love-calculator.p.rapidapi.com"
     }
 

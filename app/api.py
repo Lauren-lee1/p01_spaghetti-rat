45810dac985ded9832d5eb1b1670f31d551572f8
Lauren Lee <llee30@stuy.edu>
2022-12-19 12:49:23 -0500

    api

--- app/api.py
+++ app/api.py
@@ -7,7 +7,7 @@ def love_calculator(name1, name2):
 
     key = ""
     try:
-        with open("app/key_love_calculator.txt", "r") as file:
+        with open("keys/key_love_calculator.txt", "r") as file:
             key = file.read().strip()
     except:
         return(0)
